05fb575dfb6fc973556b6bc32d395b1f0886bd80
Muhammad Usman <muhammad.usman@tkxel.com>
2013-09-30 10:43:15 +0500

    permalink:string Migration Added

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130930051937) do
+ActiveRecord::Schema.define(version: 20130930054032) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "permalink"
   end
 
 end
